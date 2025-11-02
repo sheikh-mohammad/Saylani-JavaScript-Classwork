8a3ba2a9f72f09796c12f3083a70ae91c3663101
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-02 21:03:54 +0500

    Added Question 2

---
+++ Question 2/script.js
@@ -0,0 +1,10 @@
+var age = +prompt("Enter Your Age");
+
+if (age < 18) {
+    console.log("Underage");
+} else if (age > 65) {
+    console.log("Senior Citizen");
+}
+else if (age >= 18) {
+    console.log("Adult");
+}
