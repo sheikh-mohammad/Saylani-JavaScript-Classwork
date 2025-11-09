6db9cf037956560d26ef846ab0b33a840d82ea76
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-09 20:17:38 +0500

    Update script.js

--- Class 2/Question 2/script.js
+++ Class 2/Question 2/script.js
@@ -3,6 +3,5 @@ let fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
 console.log(fruits.slice(fruits.indexOf("Banana"), fruits.indexOf("Date") + 1));
 
 fruits.splice(2, 2, "Grape", "Honeydew");
-console.log(fruits);
 
 fruits.push("Fig", "Grapes");
