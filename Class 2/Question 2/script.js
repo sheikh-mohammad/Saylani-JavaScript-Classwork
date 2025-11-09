eb7d5d06494f9666f4ebc48787b3a55e6a12062f
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-09 20:23:14 +0500

    Update script.js

--- Class 2/Question 2/script.js
+++ Class 2/Question 2/script.js
@@ -1,15 +1,21 @@
 let fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
 
+// Slice
 console.log(fruits.slice(fruits.indexOf("Banana"), fruits.indexOf("Date") + 1));
 
+// Splice
 fruits.splice(2, 2, "Grape", "Honeydew");
 
+// Push
 fruits.push("Fig", "Grapes");
 
+// Pop
 fruits.pop();
 
+// Shift
 fruits.shift();
 
+// Unshift
 fruits.unshift("Avocado");
 
 console.log(fruits.indexOf("Honeydew"));
